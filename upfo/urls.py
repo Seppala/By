eb4effe60c643483218aa1e72f99368061824d6d
Riku <riku@seppa.la>
2012-04-06 11:03:38 +0300

    rolled back to the most advanced , 'added context processor...'

--- upfo/urls.py
+++ upfo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
+import settings
 
 from upfoMain.views import home, done, logout, error, channel, printFriends, friendson, turn_true, make_upfo, del_upfo
 
@@ -21,6 +22,7 @@ urlpatterns = patterns('',
 	url(r'^make_upfo/$', make_upfo, name='make_upfo'),
 	url(r'^del_upfo/$', del_upfo, name='del_upfo'),
 	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', 
-	{'document_root': '/Users/rvrseppala/Development/Byy/by/upfo/media'}),
+	{'document_root': settings.STATIC_URL}),
+	#{'document_root': '/Users/rvrseppala/Development/Byy/by/upfo/media'}),
 	
 )
