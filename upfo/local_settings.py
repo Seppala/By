f481e30c030f71cb373bc16a0d36480dc4e7835c
Riku <riku@seppa.la>
2012-04-06 11:18:22 +0300

    Everything should be working except the invite locally? Question is does it work remotely?

--- upfo/local_settings.py
+++ upfo/local_settings.py
@@ -1,1 +1,21 @@
-MEDIA_ROOT = '/Users/rvrseppala/Development/Byy/By/upfo/media/'
+DEBUG = True
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'upfo.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+	}
+}
+
+STATIC_ROOT = '/Users/rvrseppala/Development/Byy/By/upfo/media/'
+
+STATIC_URL = 'media/'
+
+FACEBOOK_APP_ID = '200047203426040'
+FACEBOOK_API_SECRET = 'efbddb138180cb38af95785c866f9b5d'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
